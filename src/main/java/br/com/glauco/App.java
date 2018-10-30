05becb2de2076217600e014fdcb961d38f8ef963
Glauco Carvalho <castilho.glauco@gmail.com>
2018-10-30 16:40:07 -0300

    Correção do erro ao gerar o jar

--- src/main/java/br/com/glauco/App.java
+++ src/main/java/br/com/glauco/App.java
@@ -6,6 +6,7 @@ import org.camunda.bpm.model.bpmn.Query;
 import org.camunda.bpm.model.bpmn.instance.*;
 import org.camunda.bpm.model.xml.instance.ModelElementInstance;
 import org.camunda.bpm.model.xml.type.ModelElementType;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.ByteArrayInputStream;
@@ -19,29 +20,41 @@ public class App {
 
 
     public static void main(String[] args) {
-        String startPoint = args[0];
-        String endPoint = args[1];
-        String URL = "http://localhost:8080/engine-rest/process-definition/key/invoice/xml";
 
-        //Acessing the API and parsing the data
-        RestTemplate restTemplate = new RestTemplate();
-        JsonObject jsonObject = restTemplate.getForObject(URL, JsonObject.class);
-        InputStream inputStream = new ByteArrayInputStream(jsonObject.getBpmn20Xml().getBytes(StandardCharsets.UTF_8));
-        BpmnModelInstance bpmnModelInstance = Bpmn.readModelFromStream(inputStream);
+        if(args.length < 2){
+            System.out.println("Please provide 2 parameters to serve as entry and exit points");
+
+        }else {
+
+            String startPoint = args[0];
+            String endPoint = args[1];
+            String URL = "http://localhost:8080/engine-rest/process-definition/key/invoice/xml";
+
+            //Acessing the API and parsing the data
+            RestTemplate restTemplate = new RestTemplate();
+
+            try {
+                JsonObject jsonObject = restTemplate.getForObject(URL, JsonObject.class);
+                InputStream inputStream = new ByteArrayInputStream(jsonObject.getBpmn20Xml().getBytes(StandardCharsets.UTF_8));
+                BpmnModelInstance bpmnModelInstance = Bpmn.readModelFromStream(inputStream);
 
 
-        StartEvent start = (StartEvent) bpmnModelInstance.getModelElementById("StartEvent_1");
-        UserTask task=  bpmnModelInstance.getModelElementById(startPoint);
-        ModelElementType taskType = bpmnModelInstance.getModel().getType(Task.class);
-        Collection<ModelElementInstance> taskInstances = bpmnModelInstance.getModelElementsByType(taskType);
+                StartEvent start = (StartEvent) bpmnModelInstance.getModelElementById("StartEvent_1");
+                UserTask task = bpmnModelInstance.getModelElementById(startPoint);
+                ModelElementType taskType = bpmnModelInstance.getModel().getType(Task.class);
+                Collection<ModelElementInstance> taskInstances = bpmnModelInstance.getModelElementsByType(taskType);
 
-        Query flowNodes = start.getSucceedingNodes();
+                Query flowNodes = start.getSucceedingNodes();
 
-        System.out.println("The path from " + startPoint + " to " + endPoint + " is:");
-        System.out.println("[ " + "RESULTADO AQUI " + " ]");
+                System.out.println("The path from " + startPoint + " to " + endPoint + " is:");
+                System.out.println("[ " + "RESULTADO AQUI " + " ]");
 
-        //getFlowingFlowNodes(startPoint);
+                //getFlowingFlowNodes(startPoint);
 
+            }catch (Exception e){
+                System.out.println("The resource: "+ URL + " is unavailable");
+            }
+        }
 
 
     }
